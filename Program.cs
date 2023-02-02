ca6878aee44cff4861a229ea93da7054977b36c7
Evgen Kuritsyn <chefbutcher@mail.ru>
2023-02-02 23:12:38 +0300

    дз выполнена

--- Program.cs
+++ Program.cs
@@ -2030,3 +2030,183 @@ void PrintArray(int[,] array)
         Console.WriteLine();
     }
 }*/
+/*
+// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+// N = 5 -> "1, 2, 3, 4, 5"
+
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Write("Введи число: ");
+int N  = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintNumbers(1,N));
+
+// ------------------Метод------------------
+string PrintNumbers(int start, int end){
+    if(start == end) return start.ToString();
+    return (start + " " + PrintNumbers(start + 1, end));
+}
+
+
+Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+M = 1; N = 5. -> "1, 2, 3, 4, 5"
+M = 4; N = 8. -> "4, 5, 6, 7, 8"
+
+Eka Eka, [30.01.2023 12:39]
+//Задача 63:** Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N. 
+ 
+// N = 5 -> "1, 2, 3, 4, 5" 
+ 
+// N = 6 -> "1, 2, 3, 4, 5, 6" 
+ 
+/*Console.Write("Введи число: "); 
+int N  = int.Parse(Console.ReadLine()!); 
+ 
+Console.WriteLine(PrintNumbers(1,N)); 
+ 
+// ------------------Метод------------------ 
+string PrintNumbers(int start, int end) 
+{ 
+    if(start == end) return start.ToString(); 
+    return (start + " " + PrintNumbers(start + 1, end)); 
+}*/ 
+ 
+ 
+//Задача 65: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+ 
+//M = 1; N = 5. -> "1, 2, 3, 4, 5" 
+// M = 4; N = 8. -> "4, 5, 6, 7, 8" 
+ 
+/*Console.WriteLine("Введите число  N"); 
+int N = int.Parse(Console.ReadLine()!); 
+ 
+Console.WriteLine("Введите число  M"); 
+int M = int.Parse(Console.ReadLine()!); 
+ 
+Console.WriteLine(PrintNumbers(N,M)); 
+ 
+ 
+string PrintNumbers(int start, int end) 
+{ 
+    if(start == end) return start.ToString(); 
+    return (start + " " + PrintNumbers(start + 1, end)); 
+}*/ 
+ 
+// Принять на вход число, вернуть сумму его цифр 
+ 
+/*Console.WriteLine("Введите число  N"); 
+int N = int.Parse(Console.ReadLine()!); 
+Console.WriteLine(NumberFiguresSum(N)); 
+ 
+int NumberFiguresSum(int N) 
+{ 
+    if(N == 0) return 0; 
+    return (N % 10 + NumberFiguresSum(N / 10)); 
+}*/ 
+ 
+// Возведите число А в степень Б с помощью рекурсии 
+ 
+/*Console.WriteLine("Введите число A"); 
+int A = int.Parse(Console.ReadLine()!); 
+ 
+Console.WriteLine("Введите число B"); 
+int B = int.Parse(Console.ReadLine()!); 
+ 
+Console.WriteLine(PoweredBy(A,B)); 
+ 
+// --------------method------------------- 
+ 
+int PoweredBy(int A, int 
+{ 
+    if(B == 0) return 1; 
+    if(B == 1) return A; 
+    return (A * PoweredBy(A, B - 1)); 
+}*/
+//ДОМАШНЕЕ ЗАДАНИЕ
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+/*Console.Clear();
+Console.WriteLine($"Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.");
+int n = InputNumbers("Введите n: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
+
+void PrintNumber(int n, int count)
+{
+  if (count > n) return;
+  PrintNumber(n, count + 1);
+  Console.Write(count + ", ");
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}*/
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+/*Console.Clear();
+Console.WriteLine($"Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSumm(m, n, temp=0);
+
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов= {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}*/
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+/*Console.Clear();
+Console.WriteLine($"Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}*/
